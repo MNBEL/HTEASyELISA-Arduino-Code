6a7e3672bff64811de2f3e3326d5e5d2f8183652
josiahrudge <josiahrudge@gmail.com>
2020-09-24 13:23:06 -0400

    skinnier

--- old/arduino_code_rev_8/Well.h
+++
@@ -1,9 +0,0 @@
-class Well{
-  public:
-    uint8_t well_number;
-    uint8_t ADG731_number;
-    uint8_t ADG731_select_bits;
-    uint8_t ADG731_shift_register;
-    float impedance;
-    float phase;
-};
